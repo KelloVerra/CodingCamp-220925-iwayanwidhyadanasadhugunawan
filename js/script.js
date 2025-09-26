f2bbf51f374a53c3931048017743e558dc8e56fe
KelloVerra <wayan.widhyadana.sadhugunawan@gmail.com>
2025-09-26 21:38:20 +0800

    slicing dari design figma, nambah ikon dan mengfinalisir fungsi javascript

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
 
 var namaUser = "";
 document.addEventListener('DOMContentLoaded', function () {
-    // namaUser = prompt('Masukkan nama anda: ','Guest');
-    // document.getElementById('headerGreet').innerText = `Hi ${namaUser}, Welcome to the Website`;
+    namaUser = prompt('Masukkan nama anda: ','');
+    namaUser = namaUser.trim().length > 0 ? ` ${namaUser}` : '';
+    document.getElementById('headerGreet').innerHTML = `Hi${namaUser}, <br> Ready to <span class="text-[#6933FF]">Quantum-Enhance</span> your future?`;
 })
 
 function validateContactUsForm() {
 
+    let error = false;
     document.getElementById('inputNamaError').classList.add("hidden");
     document.getElementById('inputTglLhrError').classList.add("hidden");
     document.getElementById('inputKelaminError').classList.add("hidden");
     document.getElementById('inputPesanError').classList.add("hidden");
 
     const errorElement = (name, msg) => {
+        error = true;
         document.getElementById(name).classList.remove("hidden");
         document.getElementById(name).innerText = msg;
     }
@@ -26,4 +29,8 @@ function validateContactUsForm() {
     if(tanggallahir.trim().length == 0) errorElement('inputTglLhrError','Tanggal Lahir harus diisi');
     if(kelamin === null) errorElement('inputKelaminError','Jenis Kelamin harus diisi');
     if(pesan.trim().length == 0) errorElement('inputPesanError','Pesan harus diisi');
+
+    if(error) return;
+
+    alert(`Thanks for your feedback, ${nama}!`);
 }
